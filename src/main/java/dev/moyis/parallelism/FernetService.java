8d9b9561e680e89e1b0eb6770bab29c40555974b
Fausto Moya <faustomoya99@gmail.com>
2025-06-13 17:03:08 +0200

    feature: specify executor service

--- src/main/java/dev/moyis/parallelism/FernetService.java
+++ src/main/java/dev/moyis/parallelism/FernetService.java
@@ -8,12 +8,16 @@ import dev.moyis.parallelism.model.TrashCan;
 import dev.moyis.parallelism.model.Workstation;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class FernetService {
   private final Drawer drawer;
   private final Fridge fridge;
   private final TrashCan trashCan;
   private final Workstation workstation;
+
+  private final Executor executor = Executors.newVirtualThreadPerTaskExecutor();
 
   public FernetService(Drawer drawer, Fridge fridge, TrashCan trashCan, Workstation workstation) {
     this.drawer = drawer;
@@ -23,14 +27,14 @@ public class FernetService {
   }
 
   public Optional<FernetCola> prepareFernetCola() {
-    var glassMadeOfBottle = CompletableFuture.supplyAsync(this::createGlass);
-    var pourFernetTask = CompletableFuture.supplyAsync(fridge::getFernet)
+    var glassMadeOfBottle = CompletableFuture.supplyAsync(this::createGlass, executor);
+    var pourFernetTask = CompletableFuture.supplyAsync(fridge::getFernet, executor)
         .thenAcceptAsync(fernet -> workstation.pourFernet(fernet, glassMadeOfBottle.join()));
 
-    var pourIceTask = CompletableFuture.supplyAsync(fridge::getIce)
+    var pourIceTask = CompletableFuture.supplyAsync(fridge::getIce, executor)
         .thenAcceptAsync(ice -> workstation.pourIce(ice, glassMadeOfBottle.join()));
 
-    var pourColaTask = CompletableFuture.supplyAsync(fridge::getCola)
+    var pourColaTask = CompletableFuture.supplyAsync(fridge::getCola, executor)
         .thenAcceptAsync(cola -> workstation.pourCola(cola, glassMadeOfBottle.join()));
 
     CompletableFuture.allOf(pourFernetTask, pourColaTask, pourIceTask).join();
@@ -38,9 +42,9 @@ public class FernetService {
   }
 
   private GlassMadeOfBottle createGlass() {
-    var knife = CompletableFuture.supplyAsync(drawer::getKnife);
-    var lighter = CompletableFuture.supplyAsync(drawer::getLighter);
-    var emptyBottle = CompletableFuture.supplyAsync(trashCan::searchEmptyBottle);
+    var knife = CompletableFuture.supplyAsync(drawer::getKnife, executor);
+    var lighter = CompletableFuture.supplyAsync(drawer::getLighter, executor);
+    var emptyBottle = CompletableFuture.supplyAsync(trashCan::searchEmptyBottle, executor);
     return workstation.cutBottle(knife.join(), lighter.join(), emptyBottle.join());
   }
 }
